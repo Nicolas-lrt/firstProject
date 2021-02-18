f8591862072a0d50480e5a5a5a496316fb2f7f26
Nicolas Laurent <nicolas.laurent@universite-paris-saclay.fr>
2021-02-18 17:10:41 +0100

    navigation ok

--- produit/urls.py
+++ produit/urls.py
@@ -2,5 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('professional-list', views.proList, name='proList'),
+    path('user-profile', views.userProfile1, name='userProfile1'),
+    path('blog-post', views.blogPost, name='blogPost'),
+    path('blog', views.blogPage, name='blogPage'),
+    path('gui-kit', views.guiKit, name='guiKit'),
+    path('profile-company', views.profileCompanyPage, name='profileCompanyPage'),
+    path('profile-company_no-tabs', views.profileCompanyPageNoTabs, name='profileCompanyPageNoTabs'),
+    path('network', views.networkPage, name='networkPage'),
+    path('network-hotkeys', views.networkHotkeysPage, name='networkHotkeysPage'),
+    path('network-followers', views.networkFollowersPage, name='networkFollowersPage'),
+    path('network-following', views.networkFollowingPage, name='networkFollowingPage'),
 ]
--- produit/views.py
+++ produit/views.py
@@ -7,6 +7,49 @@ from client.models import Client
 # Create your views here.
 # @login_required(login_url='acces')
 def home(request):
-    clients = Client.objects.all()
-    context = {'clients': clients}
-    return render(request, 'index.html', context)
+    return render(request, 'index.html')
+
+
+def proList(request):
+    return render(request, 'listing-filter.html')
+
+
+def userProfile1(request):
+    return render(request, 'user-profile(layout-1).html')
+
+
+def blogPost(request):
+    return render(request, 'blog-post.html')
+
+
+def blogPage(request):
+    return render(request, 'blog.html')
+
+
+def guiKit(request):
+    return render(request, 'gui-kit.html')
+
+
+def profileCompanyPage(request):
+    return render(request, 'profile_company.html')
+
+
+def profileCompanyPageNoTabs(request):
+    return render(request, 'profile_company-no-tabs.html')
+
+
+def networkPage(request):
+    return render(request, 'user-dashboard(connections)(hotkeys-disabled).html')
+
+
+def networkHotkeysPage(request):
+    return render(request, 'user-dashboard(connections)(hotkeys-enabled).html')
+
+
+def networkFollowersPage(request):
+    return render(request, 'user-dashboard(followers).html')
+
+
+def networkFollowingPage(request):
+    return render(request, 'user-dashboard(following).html')
+
