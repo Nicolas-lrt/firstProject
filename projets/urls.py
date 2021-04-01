131d1fcfe2124171c6af7ca7954aab421474369f
Nicolas Laurent <nicolas.laurent@universite-paris-saclay.fr>
2021-04-01 17:41:41 +0200

    partenaire/charte/comment investir pas finis

--- projets/urls.py
+++ projets/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    path('', views.addProject, name='addProject'),
+    path('add-project/', views.addProject, name='addProject'),
+    path('devenir-partenaire/', views.devenirPartenaire, name='devenirPartenaire'),
+    path('charte/', views.chartePage, name='chartePage'),
+    path('comment-investir/', views.commentInvestir, name='commentInvestir'),
 
 ]
--- projets/views.py
+++ projets/views.py
@@ -1,7 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 
 
+@login_required(login_url='login')
 def addProject(request):
     return render(request, 'projets/addProject.html')
+
+
+@login_required(login_url='login')
+def devenirPartenaire(request):
+    return render(request, 'projets/devenir-partenaire.html')
+
+
+@login_required(login_url='login')
+def chartePage(request):
+    return render(request, 'projets/charte.html')
+
+
+@login_required(login_url='login')
+def commentInvestir(request):
+    return render(request, 'projets/comment-investir.html')
